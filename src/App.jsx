46da4ee3c05dc31b867eacf5a8ca6882b59b7271
arun <arunpmanoj12@gmail.com>
2025-09-03 22:24:07 +0530

    added style for mouse pointer

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,8 @@ const App = () => {
       <Teams/>
       <ContactUs/>
       <Footer theme={theme}/>
-      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'>
+      <div ref={outlineRef} className='fixed top-0 left-0 h-10 w-10 rounded-full border border-primary pointer-events-none z-[9999]'
+      style={{transition:'transform 0.1s ease-out' }}></div>
       </div>
       {/* Custom Cursor Dot */}
       <div ref={dotRef} className='fixed top-0 left-0 h-3 w-3 rounded-full bg-primary pointer-events-none z-[9999]'>
