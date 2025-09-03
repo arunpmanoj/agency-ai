319a2b065ad08fb76473215ccb98a75b2b2f597f
arun <arunpmanoj12@gmail.com>
2025-09-03 20:05:40 +0530

    created the bottom of footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,6 +26,17 @@ const Footer = ({theme}) => {
             </div>
         </div>
       </div>
+      <hr className='border-gray-300 dark:border-gray-600 my-6'/>
+      {/*footer bottom*/}
+     <div className='pb-6 text-sm text-gray-500 flex justify-center sm:justify-between gap-4 flex-wrap'>
+        <p>Copyright 2025  - All Rights Reserved.</p>
+        <div className='flex items-center justify-between gap-4'>
+            <img src={assets.facebook_icon} alt=""/>
+            <img src={assets.twitter_icon} alt=""/>
+            <img src={assets.instagram_icon} alt=""/>
+            <img src={assets.linkedin_icon} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
