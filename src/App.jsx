e2ea8b3bd86f9c677a065657967af37855afa9fc
arun <arunpmanoj12@gmail.com>
2025-09-03 22:14:53 +0530

    added the functionality of mouse pointer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useRef } from "react";
+import React, { useState,useRef,useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/hero";
 import TrustedBy from "./components/TrustedBy";
@@ -13,6 +13,34 @@ const App = () => {
   const dotRef = useRef(null)
   const outlineRef = useRef (null)
 
+  // Refs for custom cursor Position tracking
+  const mouse = useRef({x: 0, y: 0})
+  const position = useRef({x: 0, y:0})
+
+  useEffect(() => {
+  const handleMouseMove = (e) => {
+    mouse.current.x = e.clientX;
+    mouse.current.y = e.clientY;
+  };
+
+  document.addEventListener("mousemove", handleMouseMove);
+
+  const animate = () => {
+    position.current.x += (mouse.current.x - position.current.x) * 0.1;
+    position.current.y += (mouse.current.y - position.current.y) * 0.1;
+
+    if (dotRef.current && outlineRef.current) {
+      dotRef.current.style.transform = `translate3d(${mouse.current.x - 6}px, ${mouse.current.y - 6}px, 0)`;
+      outlineRef.current.style.transform = `translate3d(${position.current.x - 20}px, ${position.current.y - 20}px, 0)`;
+    }
+    requestAnimationFrame(animate)
+  }
+  animate()
+  return ()=>{
+    document.removeEventListener('mousemove',handleMouseMove)
+  }
+  },[])
+  
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
   return (
