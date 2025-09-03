02d938bc078b4e80ee1f5fa72db1a72ba40687d2
arun <arunpmanoj12@gmail.com>
2025-09-03 20:38:58 +0530

    basic creation of cursor effects

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ import ContactUs from "./components/ContactUs";
 import {Toaster} from 'react-hot-toast'
 import Footer from "./components/Footer";
 const App = () => {
+
+  const dotRef = useRef(null)
+  const outlineRef = useRef (null)
+
   const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light');
 
   return (
@@ -22,7 +26,13 @@ const App = () => {
       <Teams/>
       <ContactUs/>
       <Footer theme={theme}/>
-    </div>
+      {/* custom cursor ring */}
+      <div ref={outlineRef} className="fixed top-0 left-0 h-10 w-10 rounded-full border
+       border-primary pointer-events-none z-[9999]" div></div>
+      {/* Custom Cursor Dot */}
+      <div ref={dotRef} className='fixed top-o left-o h-3 w-3 rounded-full bg-primary pointer-events-none
+       z-[9999]'div></div>
+      </div>
   );
 };
 export default App;
