be5e5b47855742636dfac28e9cdd16cb67bfd61b
arun <arunpmanoj12@gmail.com>
2025-09-03 19:48:42 +0530

    created the left section of footer(div)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import assets from '../assets/assets'
 
-const Footer = () => {
+const Footer = ({theme}) => {
   return (
     <div className='bg-slate-50 dark:bg-gray-900 pt-10 sm:pt-10 mt-20 sm:mt-40 px-4 sm:px-10 lg:px-24 xl:px-40'>
       {/* footer top */}
       <div className='flex justify-between lg:items-center max-lg:flex-col gap-10'>
-        <div>
-          <img src={assets.logo} className='w-32 sm:w-44' alt="" />
+        <div className='space-y-5 text-sm text-gray-700 dark:text-gray-400'>
+          <img src={ theme ==='dark' ? assets.logo_dark : assets.logo} className='w-32 sm:w-44' alt="" />
           <p className='max-w-md'>From strategy to execution, we craft digital solutions that move your business forward.</p>
-          <ul>
+          <ul className='flex gap-8'>
             <li><a className='hover:text-primary' href="#hero">Home</a></li>
             <li><a className='hover:text-primary' href="#services">Services</a></li>
             <li><a className='hover:text-primary' href="#our-work">Our Work</a></li>
